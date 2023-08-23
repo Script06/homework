c5a0aa591aa2febd91f24f7033ce40fc480a1f73
Alexandr Gololobov <gololobovac@gmail.com>
2023-08-24 00:46:13 +0300

    void commit

--- 7.RailsApp_LabReport/README.md
+++
@@ -1,4 +0,0 @@
-# README
-
-Программа начинает свою работу по адресу: http://localhost:3000/lab_reports
-
--- 7.RailsApp_LabReport/app/models/lab_report.rb
+++
@@ -1,6 +0,0 @@
-class LabReport < ApplicationRecord
-  belongs_to :user
-
-  validates :title, presence: true, length: { maximum: 250 }
-  validates :description, presence: true, length: { maximum: 500 }
-end
